92ab71c54d8a26818750ff10d97363b271d31fdb
Sai Gopal Challa <saigopal.challa@gmail.com>
2025-03-11 00:20:24 +0530

    test actions - GC

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Samudbhav.
         </p>
-        <a
+        <p>
+
+          We are coming Soon !!!
+        </p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React 
+        </a> */}
       </header>
     </div>
   );
 }
+
 
 export default App;
